dec1fd5416d25a348977e3ad38a9d35a3d0cba11
Fabio <fabio@MacBook-Pro-de-Fabio.local>
2018-08-27 22:24:28 -0300

    Initial Commit

---
+++ DatingApp-SPA/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ValueComponent } from './value/value.component';
+import {HttpClientModule} from '@angular/common/http';
+import { TesteComponent } from './teste/teste.component';
+
+@NgModule({
+   declarations: [
+      AppComponent,
+      ValueComponent,
+      TesteComponent
+   ],
+   imports: [
+      BrowserModule,
+      HttpClientModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
+})
+export class AppModule { }
