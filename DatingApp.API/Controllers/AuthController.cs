3411c498ac1cfbb7ab31a511cedf8efcd51adba6
Fabio Galante Mans <fabio.mans@superdigital.com.br>
2018-08-31 18:28:00 -0300

    Validacao e debug

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
+using DatingApp.API.Dtos;
 using DatingApp.API.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -21,21 +22,23 @@ namespace DatingApp.API.Controllers
         }
 
         [HttpPost("register")]
-        public async Task<IActionResult> Register(string username, string password)
+        public async Task<IActionResult> Register([FromBody] UserForRegisterDto userForRegisterDto)
         {
-            // validate request
+            //if(!ModelState.IsValid)
+              //  return BadRequest(ModelState);
+              //Com o atributo ApiController nao precisa validar e nem o FromBody
 
-            username = username.ToLower();
+            userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
 
-            if (await _repository.UserExists(username))
+            if (await _repository.UserExists(userForRegisterDto.Username))
                 return BadRequest("Username already exists");
 
             var userToCreate = new User
             {
-                Username = username,
+                Username = userForRegisterDto.Username,
             };
 
-            var createdUser = await _repository.Register(userToCreate, password);
+            var createdUser = await _repository.Register(userToCreate, userForRegisterDto.Password);
 
             return StatusCode(201);
 
--- DatingApp.API/Models/User.cs
+++ DatingApp.API/Models/User.cs
@@ -1,11 +1,7 @@
 namespace DatingApp.API.Models
 {
     public class User
-    {
-        // Todo: fazer a aula de criação da classe User e update o banco
-
-
-        public int Id { get; set; }
+    {   public int Id { get; set; }
         public string Username { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
