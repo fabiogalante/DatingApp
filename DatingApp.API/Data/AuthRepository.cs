1040f85fb6b6c0d9c9809f4166d5813502b2d62a
Fabio <fabio@MacBook-Pro-de-Fabio.local>
2018-08-31 09:57:00 -0300

    Dto

--- DatingApp.API/Data/AuthRepository.cs
+++ DatingApp.API/Data/AuthRepository.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Data
 {
-    public class AuthRepository : IAuthRepository
+    public class AuthRepository : IAuthRepository   
     {
         private readonly DataContext _context;
 
